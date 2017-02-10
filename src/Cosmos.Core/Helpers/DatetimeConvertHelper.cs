2836d1531a8a864be48ea64eecf67ae8dc453caa
alexinea <forerunner@outlook.com>
2017-02-10 15:19:04 +0800

    Add datetime convert helper

---
+++ src/Cosmos.Core/Helpers/DatetimeConvertHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cosmos.Helpers
+{
+    /// <summary>
+    /// Datetime convert helper
+    /// </summary>
+    public static class DateTimeConvertHelper
+    {
+        /// <summary>
+        /// Convert from an object to datetime
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="defaultRet"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(object obj, DateTime defaultRet = default(DateTime))
+        {
+            if (obj == null)
+                return defaultRet;
+            DateTime ret;
+            return DateTime.TryParse(obj.ToString(), out ret) ? ret : defaultRet;
+        }
+    }
+}
