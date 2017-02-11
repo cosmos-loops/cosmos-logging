b01a259e554b5aa2618a52f54e2bd0d943b363c3
alexinea <forerunner@outlook.com>
2017-02-11 23:01:30 +0800

    Add object convert helper

---
+++ src/Cosmos.Core/Helpers/ObjectConvertHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Cosmos.Helpers
+{
+    /// <summary>
+    /// Object convert helper
+    /// </summary>
+    public static class ObjectConvertHelper
+    {
+        /// <summary>
+        /// Convert from an object to string
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ToString(object obj)
+        {
+            return obj?.ToString() ?? string.Empty;
+        }
+    }
+}
