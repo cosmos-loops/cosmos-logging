a91c62242aa635735b4de0d8956614a258040d7c
alexinea <forerunner@outlook.com>
2017-12-26 18:25:28 +0800

    Add DSA encryption provider implements for asymmetric encryption, RC4 encryption provider implements for symmetric encryption,and DSAKey and TripleDSEKey

---
+++ Encryption/src/Cosmos.Encryption/Asymmetric/DSAKey.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Cosmos.Encryption {
+    public class DSAKey {
+        /// <summary>
+        /// Rsa public key
+        /// </summary>
+        public string PublicKey { get; set; }
+
+        /// <summary>
+        /// Rsa private key
+        /// </summary>
+        public string PrivateKey { get; set; }
+
+    }
+}
---
+++ Encryption/src/Cosmos.Encryption/Symmetric/TripleDESKey.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace Cosmos.Encryption {
+    // ReSharper disable once InconsistentNaming
+    public class TripleDESKey {
+        /// <summary>
+        /// ase key
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// ase IV
+        /// </summary>
+        public string IV { get; set; }
+    }
+}
