9e8d6e0c0740f6fc2280b61e2deb42c020e31547
alexinea <forerunner@outlook.com>
2017-12-26 18:26:01 +0800

    Add unit tests for symmetric encryption providers.

---
+++ Encryption/tests/Cosmos.Encryption.NfxTests/Symmetric/DesTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace Cosmos.Encryption.Tests.Symmetric {
+    public class DesTests {
+        [Fact]
+        public void EncryptDecrypt_Test() {
+            var s = DESEncryptionProvider.Encrypt("image", "alexinea", "forerunner");
+            Assert.Equal("fJ2yrnAPaH0=", s);
+
+            var o = DESEncryptionProvider.Decrypt(s, "alexinea", "forerunner");
+            Assert.Equal("image", o);
+        }
+
+        [Fact]
+        public void EncryptDecrypt_WithSalt_Test() {
+            var s = DESEncryptionProvider.Encrypt("image", "alexinea", "forerunner", "123412341234");
+            Assert.Equal("s4h5u8hA/2Y=", s);
+
+            var o = DESEncryptionProvider.Decrypt(s, "alexinea", "forerunner", "123412341234");
+            Assert.Equal("image", o);
+        }
+
+        [Fact]
+        public void EncryptDecrypt_WithAutoCreateKey_Test() {
+            var key = DESEncryptionProvider.CreateKey();
+            var s = DESEncryptionProvider.Encrypt("image", key.Key, key.IV);
+            var o = DESEncryptionProvider.Decrypt(s, key.Key, key.IV);
+            Assert.Equal("image", o);
+        }
+
+        [Fact]
+        public void EncryptDecrypt_WithSalt_WithAutoCreateKey_Test() {
+            var key = DESEncryptionProvider.CreateKey();
+            var s = DESEncryptionProvider.Encrypt("image", key.Key, key.IV, "123412341234");
+            var o = DESEncryptionProvider.Decrypt(s, key.Key, key.IV, "123412341234");
+            Assert.Equal("image", o);
+        }
+    }
+}
---
+++ Encryption/tests/Cosmos.Encryption.NfxTests/Symmetric/Rc4Tests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Cosmos.Encryption.Tests.Symmetric {
+    public class Rc4Tests {
+        [Fact]
+        public void Encrypt() {
+            var s = RC4EncryptionProvider.Encrypt("image", "alexinea");
+            Assert.Equal("I2YRaZo=", s);
+        }
+
+        [Fact]
+        public void Decrypt() {
+            var o = RC4EncryptionProvider.Decrypt("I2YRaZo=", "alexinea");
+            Assert.Equal("image", o);
+        }
+    }
+}
