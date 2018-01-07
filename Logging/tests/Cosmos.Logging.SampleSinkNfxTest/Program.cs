d6ddb3e2cd5cf682a0d24bb9430b5332ed24d36d
alexinea <forerunner@outlook.com>
2018-01-08 00:05:53 +0800

    Add .net framework console test project

---
+++ Logging/tests/Cosmos.Logging.SampleSinkNfxTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Cosmos.Logging.Configuration;
+using Cosmos.Logging.RunsOn.Console;
+using Cosmos.Logging.Sinks.SampleLogSink;
+
+namespace Cosmos.Logging.SampleSinkNfxTest {
+    class Program {
+        static void Main(string[] args) {
+
+           // try {
+                LOGGER.Initialize().RunsOnConsole()
+                    .WriteToSampleLog()
+                    .AllDone();
+
+                var logger = LOGGER.GetLogger();
+
+                logger.Information("hello world!");
+
+                Console.WriteLine("Hello World!");
+            //}
+            //catch (Exception e) {
+            //    Console.WriteLine(e.Message);
+            //    Console.WriteLine(e.Source);
+            //    Console.WriteLine(e.StackTrace);
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
