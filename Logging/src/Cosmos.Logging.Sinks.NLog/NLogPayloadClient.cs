bddfe2439eaa0e9ba128d6159d82553ddfd8a38a
alexinea <forerunner@outlook.com>
2018-01-11 00:02:02 +0800

    Add Cosmos.Logging.Sinks.NLog for CosmosLoops Logging

---
+++ Logging/src/Cosmos.Logging.Sinks.NLog/NLogPayloadClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Cosmos.Logging.Collectors;
+using Cosmos.Logging.Core.Sinks;
+using Cosmos.Logging.Events;
+
+namespace Cosmos.Logging.Sinks.NLog {
+    public class NLogPayloadClient : ILogEventSink, ILogPayloadClient {
+        private readonly IFormatProvider _formatProvider;
+
+        public NLogPayloadClient(string name, LogEventLevel? level, IFormatProvider formatProvider = null) {
+            Name = name;
+            Level = level;
+            _formatProvider = formatProvider;
+        }
+
+        public string Name { get; set; }
+        public LogEventLevel? Level { get; set; }
+
+        public Task WriteAsync(ILogPayload payload, CancellationToken cancellationToken = default(CancellationToken)) {
+            if (payload != null) {
+                var legalityEvents = LogEventSinkFilter.Filter(payload, Level).ToList();
+                var logger = global::NLog.LogManager.GetLogger(payload.Name, payload.SourceType);
+
+                foreach (var logEvent in legalityEvents) {
+                    var message = logEvent.RenderMessage(_formatProvider);
+                    var exception = logEvent.Exception;
+                    var level = LogLevelSwitcher.Switch(logEvent.Level);
+
+                    logger.Log(level, exception, message);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Logging/src/Cosmos.Logging.Sinks.NLog/NLogPayloadClientProvider.cs
@@ -0,0 +1,17 @@
+﻿using Cosmos.Logging.Collectors;
+using Microsoft.Extensions.Options;
+
+namespace Cosmos.Logging.Sinks.NLog {
+    public class NLogPayloadClientProvider : ILogPayloadClientProvider {
+        private readonly NLogSinkSettings _settings;
+
+        public NLogPayloadClientProvider(IOptions<NLogSinkSettings> settings) {
+            _settings = settings == null ? new NLogSinkSettings() : settings.Value;
+            //todo to get called method info and iFormatProvider
+        }
+
+        public ILogPayloadClient GetClient() {
+            return new NLogPayloadClient(_settings.Name, _settings.Level);
+        }
+    }
+}
