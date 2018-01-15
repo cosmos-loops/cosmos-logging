009f5a950303f5191ef9f20e22e6127073bef4bd
alexinea <forerunner@outlook.com>
2018-01-15 23:46:52 +0800

    完成第一版模板解析引擎

--- Logging/src/Cosmos.Logging/MessageTemplates/MessageTemplateParser.cs
+++ Logging/src/Cosmos.Logging/MessageTemplates/MessageTemplateParser.cs
@@ -34,7 +34,7 @@ namespace Cosmos.Logging.MessageTemplates {
                         }
                         case '@': {
                             Skip();
-                            yield return PaeseUserDefinedParameter();
+                            yield return ParseUserDefinedParameter();
                             break;
                         }
                         case '$': {
@@ -52,12 +52,28 @@ namespace Cosmos.Logging.MessageTemplates {
                 }
             }
 
-            char Peek() => messageTemplate_chars[position_offset];
-            char Take() => messageTemplate_chars[position_offset++];
-            char Next() => position_offset + 1 == length ? char.MinValue : messageTemplate_chars[position_offset + 1];
+            char Peek() => position_offset >= length ? char.MinValue : messageTemplate_chars[position_offset];
+            char Take() => position_offset >= length ? char.MinValue : messageTemplate_chars[position_offset++];
+            char Next(int next = 1) => position_offset + next >= length ? char.MinValue : messageTemplate_chars[position_offset + next];
+            char Before(int before = 1) => position_offset - before == -1 ? char.MinValue : messageTemplate_chars[position_offset - before];
+            void Back(int back = 1) => position_offset -= back;
             void Skip(int step = 1) => position_offset += step;
             void Follow() => position = position_offset;
             int Offset() => position_offset - position;
+            bool End() => position_offset >= length;
+
+            bool EndWithCloseIdentifier() {
+                var e = messageTemplate_chars[length - 1];
+                return e == '}' || e == ' ';
+            }
+
+            bool CheckCloseIdentity(int next) => Next(next) == '}' || Next(next) == ' ' || Next(next) == char.MinValue;
+
+            void BackIfNecessary(int back, int one, int afterOne) {
+                if (CheckCloseIdentity(one) && !CheckCloseIdentity(afterOne)) {
+                    Back(back);
+                }
+            }
 
             string Merge(int _start, int _len) {
                 if (_len == 0) return string.Empty;
@@ -71,10 +87,7 @@ namespace Cosmos.Logging.MessageTemplates {
 
             TextToken ParseText() {
                 while (position_offset < length) {
-                    if (Next() == char.MinValue) {
-                        break;
-                    }
-
+                    if (Next() == char.MinValue) break;
                     if (Peek() == '}' && Next() == '}') {
                         Skip(2);
                         var start = position;
@@ -93,11 +106,188 @@ namespace Cosmos.Logging.MessageTemplates {
             }
 
             PropertyToken ParsePreferencesRender() {
-                return new PropertyToken("", index++, position, PropertyTokenTypes.PreferencesRender);
+                var colon_counter = 0;
+                var formatString = string.Empty;
+                var paramsString = string.Empty;
+                var params_flag_mode = 0;
+                var fixOriginTextLength = 3;
+                while (position_offset < length) {
+                    if (Next() == char.MinValue) break;
+                    if (Before() == '[' && Next() == ']') {
+                        Skip(2);
+                        continue;
+                    }
+
+                    if (Peek() == '}' || Peek() == ' ') {
+                        Skip();
+                        var start = position;
+                        var len = Offset();
+                        var rawText = Merge(start, len);
+                        Follow();
+                        return Touch(rawText, start);
+                    }
+
+                    if (Peek() == ':') {
+                        if (Next() == '}' || Next() == ' ') {
+                            Skip(2);
+                            var start = position;
+                            var len = Offset();
+                            var rawText = Merge(start, len);
+                            Follow();
+                            return Touch(rawText, start);
+                        }
+
+                        switch (++colon_counter) {
+                            case 1: {
+                                Skip();
+                                formatString = SeparatePropertyRawFormatText(out params_flag_mode, out fixOriginTextLength);
+                                if (End()) {
+                                    return Touch(Merge(position, Offset()), position);
+                                }
+
+                                BackIfNecessary(1, 0, 1);
+                                break;
+                            }
+                            case 2: {
+                                Skip();
+                                paramsString = SeparatePropertyRawParamsText(out fixOriginTextLength);
+                                if (End()) {
+                                    return Touch(Merge(position, Offset()), position);
+                                }
+
+                                BackIfNecessary(1, 0, 1);
+                                break;
+                            }
+                        }
+
+                        continue;
+                    }
+
+                    Skip();
+                }
+
+                fixOriginTextLength = EndWithCloseIdentifier() ? 3 : 2;
+                return Touch(MergeSurplus(position), position);
+
+                PropertyToken Touch(string raw, int start) {
+                    return new PropertyToken(raw, formatString, paramsString, index++, start, PropertyTokenTypes.PreferencesRender, params_flag_mode, fixOriginTextLength);
+                }
             }
 
-            PropertyToken PaeseUserDefinedParameter() {
-                return new PropertyToken("", index++, position, PropertyTokenTypes.UserDefinedParameter);
+            PropertyToken ParseUserDefinedParameter() {
+                var colon_counter = 0;
+                var formatString = string.Empty;
+                var paramsString = string.Empty;
+                var params_flag_mode = 0;
+                var fixOriginTextLength = 3;
+                while (position_offset < length) {
+                    if (Next() == char.MinValue) break;
+                    if (Before() == '[' && Next() == ']') {
+                        Skip(2);
+                        continue;
+                    }
+
+                    if (Peek() == '}' || Peek() == ' ') {
+                        Skip();
+                        var start = position;
+                        var len = Offset();
+                        var rawText = Merge(start, len);
+                        Follow();
+                        return Touch(rawText, start);
+                    }
+
+                    if (Peek() == ':') {
+                        if (Next() == '}' || Next() == ' ') {
+                            Skip(2);
+                            var start = position;
+                            var len = Offset();
+                            var rawText = Merge(start, len);
+                            Follow();
+                            return Touch(rawText, start);
+                        }
+
+                        switch (++colon_counter) {
+                            case 1: {
+                                Skip();
+                                formatString = SeparatePropertyRawFormatText(out params_flag_mode, out fixOriginTextLength);
+                                if (End()) {
+                                    return Touch(Merge(position, Offset()), position);
+                                }
+
+                                BackIfNecessary(1, 0, 1);
+                                break;
+                            }
+                            case 2: {
+                                Skip();
+                                paramsString = SeparatePropertyRawParamsText(out fixOriginTextLength);
+                                if (End()) {
+                                    return Touch(Merge(position, Offset()), position);
+                                }
+
+                                BackIfNecessary(1, 0, 1);
+                                break;
+                            }
+                        }
+
+                        continue;
+                    }
+
+                    Skip();
+
+                }
+
+                fixOriginTextLength = EndWithCloseIdentifier() ? 3 : 2;
+                return Touch(MergeSurplus(position), position);
+
+                PropertyToken Touch(string raw, int start) {
+                    return new PropertyToken(raw, formatString, paramsString, index++, start, PropertyTokenTypes.UserDefinedParameter, params_flag_mode, fixOriginTextLength);
+                }
+            }
+
+            string SeparatePropertyRawFormatText(out int paramsFlagMode, out int fixOriginTextLength) {
+                var start = position_offset;
+                paramsFlagMode = 0;
+                fixOriginTextLength = 3;
+                while (position_offset < length) {
+                    if (Before() == '[' && Next() == ']') {
+                        Skip(2);
+                        continue;
+                    }
+
+                    var c = Peek();
+                    if (c == char.MinValue) break;
+                    if (c == ':' || c == '}' || c == '(' || c == ' ') {
+                        paramsFlagMode = c == '(' ? 1 : 0;
+                        return Merge(start, position_offset - start);
+                    }
+
+                    Skip();
+                }
+
+                fixOriginTextLength = 2;
+                return Merge(start, position_offset - start);
+            }
+
+            string SeparatePropertyRawParamsText(out int fixOriginTextLength) {
+                var start = position_offset;
+                fixOriginTextLength = 3;
+                while (position_offset < length) {
+                    if (Before() == '[' && Next() == ']') {
+                        Skip(2);
+                        continue;
+                    }
+
+                    var c = Peek();
+                    if (c == char.MinValue) break;
+                    if (c == ':' || c == '}' || c == ' ') {
+                        return Merge(start, position_offset - start);
+                    }
+
+                    Skip();
+                }
+
+                fixOriginTextLength = 2;
+                return Merge(start, position_offset - start);
             }
         }
     }
