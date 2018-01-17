31e687ddaad6a56f80afd87b029c403373ac39d6
alexinea <forerunner@outlook.com>
2018-01-17 23:20:54 +0800

    添加标准格式化工具

---
+++ Logging/src/Cosmos.Logging/Formattings/FormatCommandFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Cosmos.Logging.Formattings.Helpers;
+
+namespace Cosmos.Logging.Formattings {
+    public static class FormatCommandFactory {
+        public static IEnumerable<Func<object, IFormatProvider, object>> CreateCommandFunc(string format = null) {
+            if (format == null || string.IsNullOrWhiteSpace(format)) yield break;
+            if (CasingHelper.Check(out var cmd1, format) && (cmd1 == 'U' || cmd1 == 'w')) {
+                yield return CasingHelper.Format()(cmd1);
+            }
+
+            if (PaddingHelper.Check(out var cmd2, out var width, format) && (cmd2 == 'r' || cmd2 == 'l')) {
+                yield return PaddingHelper.Format()(cmd2)(width);
+            }
+
+            if (JsonHelper.Check(format)) {
+                yield return JsonHelper.Format;
+            }
+        }
+
+        public static IEnumerable<FormatEvent> CreateCommandEvent(string format = null) {
+            if (format == null || string.IsNullOrWhiteSpace(format)) yield break;
+            if (CasingHelper.Check(out var cmd1, format) && (cmd1 == 'U' || cmd1 == 'w')) {
+                yield return new FormatEvent(10000, CasingHelper.Format()(cmd1));
+            }
+
+            if (PaddingHelper.Check(out var cmd2, out var width, format) && (cmd2 == 'r' || cmd2 == 'l')) {
+                yield return new FormatEvent(10001, PaddingHelper.Format()(cmd2)(width));
+            }
+
+            if (JsonHelper.Check(format)) {
+                yield return new FormatEvent(99999, JsonHelper.Format);
+            }
+        }
+    }
+}
---
+++ Logging/src/Cosmos.Logging/Formattings/FormatEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cosmos.Logging.Formattings {
+    public struct FormatEvent {
+        public int Sort { get; }
+        public Func<object, IFormatProvider, object> Command { get; }
+
+        public FormatEvent(int sort, Func<object, IFormatProvider, object> cmd) {
+            Sort = sort;
+            Command = cmd;
+        }
+    }
+}
