283ee45204d8c0d5f0f6a516de198810c4bccedf
alexinea <forerunner@outlook.com>
2018-01-19 21:52:27 +0800

    添加对格式化指令s/S的支持

--- Logging/src/Cosmos.Logging/Formattings/FormatCommandFactory.cs
+++ Logging/src/Cosmos.Logging/Formattings/FormatCommandFactory.cs
@@ -6,6 +6,11 @@ namespace Cosmos.Logging.Formattings {
     public static class FormatCommandFactory {
         public static IEnumerable<Func<object, IFormatProvider, object>> CreateCommandFunc(string format = null) {
             if (format == null || string.IsNullOrWhiteSpace(format)) yield break;
+
+            if (StringHelper.Check(out var cmd0, format) && (cmd0 == 'S' || cmd0 == 's')) {
+                yield return StringHelper.Format()(cmd0);
+            }
+
             if (CasingHelper.Check(out var cmd1, format) && (cmd1 == 'U' || cmd1 == 'w')) {
                 yield return CasingHelper.Format()(cmd1);
             }
@@ -21,16 +26,21 @@ namespace Cosmos.Logging.Formattings {
 
         public static IEnumerable<FormatEvent> CreateCommandEvent(string format = null) {
             if (format == null || string.IsNullOrWhiteSpace(format)) yield break;
+
+            if (StringHelper.Check(out var cmd0, format) && (cmd0 == 'S' || cmd0 == 's')) {
+                yield return new FormatEvent(cmd0, 9000, StringHelper.Format()(cmd0));
+            }
+
             if (CasingHelper.Check(out var cmd1, format) && (cmd1 == 'U' || cmd1 == 'w')) {
-                yield return new FormatEvent(10000, CasingHelper.Format()(cmd1));
+                yield return new FormatEvent(cmd1, 10000, CasingHelper.Format()(cmd1));
             }
 
             if (PaddingHelper.Check(out var cmd2, out var width, format) && (cmd2 == 'r' || cmd2 == 'l')) {
-                yield return new FormatEvent(10001, PaddingHelper.Format()(cmd2)(width));
+                yield return new FormatEvent(cmd2, 10001, PaddingHelper.Format()(cmd2)(width));
             }
 
             if (JsonHelper.Check(format)) {
-                yield return new FormatEvent(99999, JsonHelper.Format);
+                yield return new FormatEvent('j', 99999, JsonHelper.Format);
             }
         }
     }
--- Logging/src/Cosmos.Logging/Formattings/FormatEvent.cs
+++ Logging/src/Cosmos.Logging/Formattings/FormatEvent.cs
@@ -3,9 +3,11 @@
 namespace Cosmos.Logging.Formattings {
     public struct FormatEvent {
         public int Sort { get; }
+        public char CmdAlias { get; }
         public Func<object, IFormatProvider, object> Command { get; }
 
-        public FormatEvent(int sort, Func<object, IFormatProvider, object> cmd) {
+        public FormatEvent(char cmdAlias, int sort, Func<object, IFormatProvider, object> cmd) {
+            CmdAlias = cmdAlias;
             Sort = sort;
             Command = cmd;
         }
