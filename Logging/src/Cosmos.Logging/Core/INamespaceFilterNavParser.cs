b3b47a990d3cfe783c69723f543b9144fbc52a8a
alexinea <forerunner@outlook.com>
2018-01-27 00:42:54 +0800

    Add log event level alias mechanism

--- Logging/src/Cosmos.Logging/Core/INamespaceFilterNavParser.cs
+++
@@ -1,7 +0,0 @@
-﻿using Cosmos.Logging.Filters;
-
-namespace Cosmos.Logging.Core {
-    public interface INamespaceFilterNavParser {
-        NamespaceFilterNav Parse(string @namespace, string level, out EndValueNamespaceFilterNav endValueNode);
-    }
-}
