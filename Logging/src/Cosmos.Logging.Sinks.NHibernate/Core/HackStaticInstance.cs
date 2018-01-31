f6b4130eb3bcb87eada7be6f37b02738c21e7451
alexinea <forerunner@outlook.com>
2018-01-31 22:01:28 +0800

    Update Nh

--- Logging/src/Cosmos.Logging.Sinks.NHibernate/Core/HackStaticInstance.cs
+++ Logging/src/Cosmos.Logging.Sinks.NHibernate/Core/HackStaticInstance.cs
@@ -6,7 +6,7 @@ namespace Cosmos.Logging.Sinks.NHibernate.Core {
             if (loggingServiceProvider == null) throw new ArgumentNullException(nameof(loggingServiceProvider));
             if (settings == null) throw new ArgumentNullException(nameof(settings));
             Logging.Core.StaticInstanceOfLoggingServiceProvider.SetInstance(loggingServiceProvider);
-            global::NHibernate.LoggerProvider.SetLoggersFactory(new NHibernateLoggerWrapperFactory(null, settings.Filter));
+            global::NHibernate.LoggerProvider.SetLoggersFactory(new NHibernateLoggerWrapperFactory(loggingServiceProvider, settings.Filter));
         }
     }
 }
