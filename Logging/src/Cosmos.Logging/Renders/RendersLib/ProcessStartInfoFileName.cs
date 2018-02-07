3b2e964895e74de3401cf4537f379868ae53130a
alexinea <forerunner@outlook.com>
2018-02-07 14:55:32 +0800

    Add environment renderers and process renderers

---
+++ Logging/src/Cosmos.Logging/Renders/RendersLib/ProcessStartInfoFileName.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Cosmos.Logging.Core;
+using Cosmos.Logging.Core.Extensions;
+using Cosmos.Logging.Formattings;
+
+namespace Cosmos.Logging.Renders.RendersLib {
+    public class ProcessStartInfoFileName : BasicPreferencesRenderer {
+
+        public override string Name => "StartFileName";
+
+        private static string GetStartInfoArguments() {
+            return Process.GetCurrentProcess().StartInfo.FileName;
+        }
+
+        public override string ToString(string format, string paramsText, ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            return GetStartInfoArguments();
+        }
+
+        public override string ToString(IList<FormatEvent> formattingEvents, string paramsText,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            return formattingEvents.ToFormat(GetStartInfoArguments(), formatProvider);
+        }
+
+        public override string ToString(IList<Func<object, IFormatProvider, object>> formattingFuncs, string paramsText,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            return formattingFuncs.ToFormat(GetStartInfoArguments(), formatProvider);
+        }
+
+        public override void Render(string format, string paramsText, StringBuilder stringBuilder,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            stringBuilder.Append(ToString(format, paramsText, logEventInfo, formatProvider));
+        }
+
+        public override void Render(IList<FormatEvent> formattingEvents, string paramsText, StringBuilder stringBuilder,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            stringBuilder.Append(ToString(formattingEvents, paramsText, logEventInfo, formatProvider));
+        }
+
+        public override void Render(IList<Func<object, IFormatProvider, object>> formattingFuncs, string paramsText, StringBuilder stringBuilder,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            stringBuilder.Append(ToString(formattingFuncs, paramsText, logEventInfo, formatProvider));
+        }
+    }
+}
---
+++ Logging/src/Cosmos.Logging/Renders/RendersLib/ProcessStartTime.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Cosmos.Logging.Core;
+using Cosmos.Logging.Core.Extensions;
+using Cosmos.Logging.Formattings;
+
+namespace Cosmos.Logging.Renders.RendersLib {
+    public class ProcessStartTime : BasicPreferencesRenderer {
+
+        public override string Name => "ProcessStartTime";
+
+        private static string GetProcessStartTime(string format, string paramsText) {
+            return Process.GetCurrentProcess().StartTime.ToString(FixedFormat(format, paramsText));
+        }
+
+        private static string FixedFormat(string format, string paramsText) {
+            return string.IsNullOrWhiteSpace(format)
+                ? string.IsNullOrWhiteSpace(paramsText)
+                    ? "yyyy/MM/dd HH:mm:ss.ffff, z"
+                    : paramsText
+                : format;
+        }
+
+        public override string ToString(string format, string paramsText, ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            return GetProcessStartTime(format, paramsText);
+        }
+
+        public override string ToString(IList<FormatEvent> formattingEvents, string paramsText,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            return formattingEvents.ToFormat(GetProcessStartTime(null, paramsText), formatProvider);
+        }
+
+        public override string ToString(IList<Func<object, IFormatProvider, object>> formattingFuncs, string paramsText,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            return formattingFuncs.ToFormat(GetProcessStartTime(null, paramsText), formatProvider);
+        }
+
+        public override void Render(string format, string paramsText, StringBuilder stringBuilder,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            stringBuilder.Append(ToString(format, paramsText, logEventInfo, formatProvider));
+        }
+
+        public override void Render(IList<FormatEvent> formattingEvents, string paramsText, StringBuilder stringBuilder,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            stringBuilder.Append(ToString(formattingEvents, paramsText, logEventInfo, formatProvider));
+        }
+
+        public override void Render(IList<Func<object, IFormatProvider, object>> formattingFuncs, string paramsText, StringBuilder stringBuilder,
+            ILogEventInfo logEventInfo = null, IFormatProvider formatProvider = null) {
+            stringBuilder.Append(ToString(formattingFuncs, paramsText, logEventInfo, formatProvider));
+        }
+    }
+}
