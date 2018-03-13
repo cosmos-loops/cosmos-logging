126a9a9777b3eb7d85ae26aebf4f44f88e9736b8
alexinea <forerunner@outlook.com>
2018-03-14 02:15:59 +0800

    Add formatting helpers

---
+++ Logging/src/Cosmos.Logging.Sinks.File/Formatings/Helpers/Internals/InternalCallerInfoHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using Cosmos.Logging.Events;
+
+namespace Cosmos.Logging.Sinks.File.Formatings.Helpers.Internals {
+    internal static class InternalCallerInfoHelper {
+        public static string GetCallerInfoString(LogEvent logEvent) {
+            var caller = logEvent.CallerInfo;
+            switch (logEvent.Level) {
+                case LogEventLevel.Verbose:
+                case LogEventLevel.Debug:
+                case LogEventLevel.Information:
+                    return string.IsNullOrWhiteSpace(caller.MemberName)
+                        ? string.Empty
+                        : caller.MemberName;
+
+                case LogEventLevel.Warning:
+                case LogEventLevel.Error:
+                case LogEventLevel.Fatal:
+                    return $"{caller.FilePath}({caller.LineNumber}):{caller.MemberName}";
+            }
+
+            return string.Empty;
+        }
+
+        public static string GetCallerInfoString(LogEvent logEvent, string commandAlias2) {
+            var caller = logEvent.CallerInfo;
+            switch (logEvent.Level) {
+                case LogEventLevel.Verbose:
+                case LogEventLevel.Debug:
+                case LogEventLevel.Information:
+                    return commandAlias2.Contains("m") && !string.IsNullOrWhiteSpace(caller.MemberName)
+                        ? caller.MemberName
+                        : string.Empty;
+
+                case LogEventLevel.Warning:
+                case LogEventLevel.Error:
+                case LogEventLevel.Fatal:
+                    var stringBuilder = new StringBuilder();
+                    if (commandAlias2.Contains("f"))
+                        stringBuilder.Append(caller.FilePath);
+                    if (commandAlias2.Contains("n"))
+                        stringBuilder.Append($"({caller.LineNumber})");
+                    if (commandAlias2.Contains("m"))
+                        stringBuilder.Append(stringBuilder.Length == 0 ? caller.MemberName : $":{caller.MemberName}");
+                    return stringBuilder.Length == 0 ? string.Empty : stringBuilder.ToString();
+
+                default: return string.Empty;
+            }
+        }
+    }
+}
