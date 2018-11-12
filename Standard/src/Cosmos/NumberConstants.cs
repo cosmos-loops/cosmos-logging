1084eda4e8cbee2266d3c76ef2b687d81367ffa4
alexinea <forerunner@outlook.com>
2018-11-12 23:46:41 +0800

    Add NumberConstants and update cs project files.

---
+++ Standard/src/Cosmos/NumberConstants.cs
@@ -0,0 +1,9 @@
+namespace Cosmos
+{
+    public static class NumberConstants
+    {
+        public static int One => 1;
+        public static int IntMax => int.MaxValue;
+        public static int IntMin => int.MinValue;
+    }
+}
