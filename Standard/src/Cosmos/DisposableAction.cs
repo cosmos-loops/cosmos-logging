caabb930864e0a5dc7363717b8c51541153ce958
alexinea <forerunner@outlook.com>
2019-01-17 17:11:31 +0800

    Add Type several utils for cosmos.core

--- Standard/src/Cosmos/DisposableAction.cs
+++ Standard/src/Cosmos/DisposableAction.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 
-namespace Cosmos {
-    public sealed class DisposableAction : IDisposable {
+namespace Cosmos
+{
+    public sealed class DisposableAction : IDisposable
+    {
         private readonly Action _action;
 
-        public DisposableAction(Action action) {
+        public DisposableAction(Action action)
+        {
             _action = action;
         }
 
         /// <summary>
         /// 释放
         /// </summary>
-        public void Dispose() {
+        public void Dispose()
+        {
             _action?.Invoke();
         }
     }
