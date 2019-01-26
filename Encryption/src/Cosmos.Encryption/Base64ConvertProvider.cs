36cdd590a63d5cdd507f26aa401409927b876052
alexinea <forerunner@outlook.com>
2019-01-26 12:01:48 +0800

    Add RCX, ThreeRCX and Base64Url providers.

--- Encryption/src/Cosmos.Encryption/Base64ConvertProvider.cs
+++ Encryption/src/Cosmos.Encryption/Base64ConvertProvider.cs
@@ -1,27 +1,42 @@
 ﻿using System;
 using System.Text;
 
-namespace Cosmos.Encryption {
-    public static class Base64ConvertProvider {
+/*
+ * Base64UrlString Reference to:
+ *     https://github.com/toolgood/RCX/blob/master/ToolGood.RcxTest/ToolGood.RcxTest/Base64.cs
+ *     Author: ToolGood
+ */
+
+namespace Cosmos.Encryption
+{
+    public static class Base64ConvertProvider
+    {
+        private const string BASE64 = "===========================================+=+=/0123456789=======ABCDEFGHIJKLMNOPQRSTUVWXYZ====/=abcdefghijklmnopqrstuvwxyz=====";
+
         /// <summary>
-        /// Base64 encryption.
-        /// Author: Seay Xu
-        ///     https://github.com/godsharp/GodSharp.Encryption/blob/master/src/GodSharp.Shared/Encryption/Base64.cs
-        /// Editor: AlexLEWIS
+        /// Convert from bytes to base64 string
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static string ToBase64String(byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// Base64 encode.
         /// </summary>
         /// <param name="data">The string to be encrypted,not null.</param>
         /// <param name="encoding">The <see cref="T:System.Text.Encoding"/>,default is Encoding.UTF8.</param>
         /// <returns>The encrypted string.</returns>
-        public static string Encode(string data, Encoding encoding = null) {
-            if (data == null) {
-                throw new ArgumentNullException(nameof(data));
-            }
-
-            if (encoding == null) {
+        public static string ToBase64String(string data, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
                 encoding = Encoding.UTF8;
             }
 
-            return Convert.ToBase64String(encoding.GetBytes(data));
+            return ToBase64String(encoding.GetBytes(data));
         }
 
         /// <summary>
@@ -30,16 +45,74 @@ namespace Cosmos.Encryption {
         /// <param name="data">The string to be decrypted,not null.</param>
         /// <param name="encoding">The <see cref="T:System.Text.Encoding"/>,default is Encoding.UTF8.</param>
         /// <returns>The decrypted string.</returns>
-        public static string Decode(string data, Encoding encoding = null) {
-            if (data == null) {
+        public static string FromBase64String(string data, Encoding encoding = null)
+        {
+            if (data == null)
+            {
                 throw new ArgumentNullException(nameof(data));
             }
 
-            if (encoding == null) {
+            if (encoding == null)
+            {
                 encoding = Encoding.UTF8;
             }
 
             return encoding.GetString(Convert.FromBase64String(data));
+        }
+
+        /// <summary>
+        /// Convert from string to base64url string
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static string ToBase64UrlString(byte[] bytes)
+        {
+            var result = new StringBuilder(Convert.ToBase64String(bytes).TrimEnd('='));
+            result.Replace('+', '-');
+            result.Replace('/', '_');
+            return result.ToString();
+        }
+
+        /// <summary>
+        /// Convert from string to base64url string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static string ToBase64UrlString(string str, Encoding encoding)
+        {
+            return ToBase64UrlString(encoding.GetBytes(str));
+        }
+
+        /// <summary>
+        /// Convert from base64url string to string
+        /// </summary>
+        /// <param name="base64UrlString"></param>
+        public static byte[] FromBase64UrlString(string base64UrlString)
+        {
+            var sb = new StringBuilder();
+            foreach (var c in base64UrlString)
+            {
+                if ((int) c >= 128) continue;
+                var k = BASE64[c];
+                if (k == '=') continue;
+                sb.Append(k);
+            }
+
+            var len = sb.Length;
+            int padChars = (len % 4) == 0 ? 0 : (4 - (len % 4));
+            if (padChars > 0) sb.Append(string.Empty.PadRight(padChars, '='));
+            return Convert.FromBase64String(sb.ToString());
+        }
+
+        /// <summary>
+        /// Convert from base64url string to string
+        /// </summary>
+        /// <param name="base64UrlString"></param>
+        /// <param name="encoding"></param>
+        public static string FromBase64UrlString(string base64UrlString, Encoding encoding)
+        {
+            return encoding.GetString(FromBase64UrlString(base64UrlString));
         }
     }
 }
--- Encryption/src/Cosmos.Encryption/Symmetric/RC4EncryptionProvider.cs
+++ Encryption/src/Cosmos.Encryption/Symmetric/RC4EncryptionProvider.cs
@@ -4,38 +4,52 @@ using System.Text;
 using Cosmos.Encryption.Abstractions;
 
 // ReSharper disable once CheckNamespace
-namespace Cosmos.Encryption {
+namespace Cosmos.Encryption
+{
     /// <summary>
     /// Symmetric/RC4 encryption.
     /// Reference: https://bitlush.com/blog/rc4-encryption-in-c-sharp
     /// </summary>
     // ReSharper disable once InconsistentNaming
-    public sealed class RC4EncryptionProvider : ISymmetricEncyption {
+    public sealed class RC4EncryptionProvider : ISymmetricEncyption
+    {
         private RC4EncryptionProvider() { }
 
-        public static string Encrypt(string data, string key, Encoding encoding = null) {
+        public static string Encrypt(string data, string key, Encoding encoding = null)
+        {
             if (encoding == null) encoding = Encoding.UTF8;
             return Convert.ToBase64String(EncryptCore(encoding.GetBytes(data), encoding.GetBytes(key)));
         }
 
-        public static byte[] Encrypt(byte[] data, byte[] key) {
+        public static string Encrypt(byte[] data, string key, Encoding encoding = null)
+        {
+            if (encoding == null) encoding = Encoding.UTF8;
+            return Convert.ToBase64String(EncryptCore(data, encoding.GetBytes(key)));
+        }
+
+        public static byte[] Encrypt(byte[] data, byte[] key)
+        {
             return EncryptCore(data, key);
         }
 
-        public static string Decrypt(string data, string key, Encoding encoding = null) {
+        public static string Decrypt(string data, string key, Encoding encoding = null)
+        {
             if (encoding == null) encoding = Encoding.UTF8;
             return encoding.GetString(EncryptCore(Convert.FromBase64String(data), encoding.GetBytes(key)));
         }
 
-        public static byte[] Decrypt(byte[] data, byte[] key) {
+        public static byte[] Decrypt(byte[] data, byte[] key)
+        {
             return EncryptCore(data, key);
         }
 
-        private static byte[] EncryptCore(byte[] data, byte[] key) {
+        private static byte[] EncryptCore(byte[] data, byte[] key)
+        {
             var s = Initalize(key);
             int i = 0, j = 0;
 
-            return data.Select(b => {
+            return data.Select(b =>
+            {
                 i = (i + 1) & 255;
                 j = (j + s[i]) & 255;
                 Swap(s, i, j);
@@ -43,9 +57,11 @@ namespace Cosmos.Encryption {
             }).ToArray();
         }
 
-        private static byte[] Initalize(byte[] key) {
+        private static byte[] Initalize(byte[] key)
+        {
             var s = Enumerable.Range(0, 256).Select(i => (byte) i).ToArray();
-            for (int i = 0, j = 0; i < 256; i++) {
+            for (int i = 0, j = 0; i < 256; i++)
+            {
                 j = (j + key[i % key.Length] + s[i]) & 255;
                 Swap(s, i, j);
             }
@@ -53,7 +69,8 @@ namespace Cosmos.Encryption {
             return s;
         }
 
-        private static void Swap(byte[] s, int i, int j) {
+        private static void Swap(byte[] s, int i, int j)
+        {
             var b = s[i];
             s[i] = s[j];
             s[j] = b;
