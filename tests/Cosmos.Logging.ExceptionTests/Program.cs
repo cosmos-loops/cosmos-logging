8b9d01d8f3b022b558a71a9ba00dc2a6e2b6b250
alexinea <forerunner@outlook.com>
2019-11-22 15:23:30 +0800

    Add test/demo porject for Cosmos.Extensions/Exception (exceptions extension/enricher).

---
+++ tests/Cosmos.Logging.ExceptionTests/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Cosmos.Logging.Configurations;
+using Cosmos.Logging.Events;
+
+namespace Cosmos.Logging.ExceptionTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LOGGER.Initialize().RunsOnConsole(o => o.EnableDisplayCallerInfo(ThreeValuedBoolean.False).EnableDisplayEventIdInfo(true))
+                    .AddSampleLog(s => s.UseMinimumLevel(LogEventLevel.Error).EnableDisplayCallerInfo(true))
+                    .AddExceptionsIntegration()
+                    .AllDone();
+
+                var logger = LOGGER.GetLogger<Program>();
+
+                var inner = new ArgumentNullException("test", "Inner exception");
+                var exception = new InvalidOperationException("Invalid operation exception for test.", inner);
+                
+
+                logger.LogError(exception, @"
+Gets exception '{$Exception:U:N}'
+Inner exception '{$Exception:U:U/N}'
+Type: {$Exception::type}
+Message: {$Exception}
+Inner Message: {$Exception::unwrap/M}
+Detail: {$Exception:j:D}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source);
+                Console.WriteLine(e.StackTrace);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
