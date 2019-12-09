13699f4f2897fe2b3858b5b777c0a159feeb2100
alexinea <forerunner@outlook.com>
2019-12-09 17:06:20 +0800

    Add CorrelationId extensions for LogTraceId

---
+++ src/Cosmos.Logging.Extensions.CorrelationId/CorrelationIdGenerator.cs
@@ -0,0 +1,17 @@
+﻿using CorrelationId;
+using Cosmos.Logging.Trace;
+
+namespace Cosmos.Logging.Extensions.CorrelationId {
+    public class CorrelationIdGenerator : ILogTraceIdGenerator {
+        private readonly ICorrelationContextAccessor _accessor;
+
+        /// <inheritdoc />
+        public CorrelationIdGenerator(ICorrelationContextAccessor correlationContextAccessor) {
+            _accessor = correlationContextAccessor;
+        }
+
+        public string Create() {
+            return _accessor.CorrelationContext.CorrelationId;
+        }
+    }
+}
